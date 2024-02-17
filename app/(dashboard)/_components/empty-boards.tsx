ca97504d556ca8db9d83378fbb6a7a0e3a6cb1a2
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-02-17 15:05:18 +0530

    search querying done

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -7,8 +7,10 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const EmptyBoards = () => {
+    const router = useRouter();
     const { organization } = useOrganization();
     const {mutate, pending} = useApiMutation(api.board.create)
 
@@ -22,6 +24,7 @@ export const EmptyBoards = () => {
         .then((id)=>{
             toast.success("Board created!")
             // TODO: Redirect to board/${id}
+            router.push(`/board/${id}`);
         })
         .catch((error)=> toast.error("Failed to create board!"));
     }
--- convex/board.ts
+++ convex/board.ts
@@ -54,6 +54,20 @@ export const remove = mutation({
         }
 
         // TODO: later check to delete favorite relation as well 
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q)=>
+        q
+        .eq("userId", userId)
+        .eq("boardId", args.id)
+        )
+        .unique();
+
+        if (existingFavorite) {
+            await ctx.db.delete(existingFavorite._id);
+        }
 
         await ctx.db.delete(args.id);
     }
@@ -113,10 +127,9 @@ export const favorite = mutation({
 
         const existingFavorite = await ctx.db
             .query("userFavorites")
-            .withIndex("by_user_board_org", (q) => q
+            .withIndex("by_user_board", (q) => q
                 .eq("userId", userId)
                 .eq("boardId", board._id)
-                .eq("orgId", args.orgId)
             )
             .unique();
 
